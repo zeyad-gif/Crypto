0cdccb413e00e2c7f31fca85aedec56c71a0d238
zeyad-gif <zeyadgamegx7@gmail.com>
2026-05-17 22:05:02 +0300

    Delete Algorithms directory

--- Algorithms/hash.js
+++
@@ -1,7 +0,0 @@
-window.sha256Hash = async function (text) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(text);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
-};
